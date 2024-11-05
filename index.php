b75df6e092837db2d11f12c7910485e96493a047
ituki yamanaka <yamanakasan@icloud.com>
2024-11-05 22:55:22 +0900

    calendarの機能追加

--- index.php
+++ index.php
@@ -24,18 +24,28 @@
 <body>
     <div class="container">
         <h2>
-            <a href="?ym=<?php echo date('Y-m', strtotime('-1 month')); ?>">&lt;</a>
-            <?php echo date('Y年n月'); ?>
-            <a href="?ym=<?php echo date('Y-m', strtotime('+1 month')); ?>">&gt;</a>
+            <!-- 前月のリンク -->
+            <a href="?ym=<?php echo date('Y-m', strtotime('-1 month', strtotime($currentMonth))); ?>">&lt;</a>
+            
+            <!-- 現在の年月を表示 -->
+            <?php
+            // 現在の月と年をGETパラメータで取得（無ければデフォルトで現在の年月）
+            $currentMonth = isset($_GET['ym']) ? $_GET['ym'] : date('Y-m'); 
+            
+            // 年月表示
+            echo date('Y年n月', strtotime($currentMonth));
+            ?>
+            
+            <!-- 次月のリンク -->
+            <a href="?ym=<?php echo date('Y-m', strtotime('+1 month', strtotime($currentMonth))); ?>">&gt;</a>
         </h2>
 
         <?php
+        // 月と年を抽出
+        list($year, $month) = explode('-', $currentMonth);
+
         // カレンダー表示用のPHPをインクルード
         include 'app.php';
-        
-        // 現在の月と年を取得
-        $month = date('n'); 
-        $year = date('Y'); 
 
         // カレンダー表示
         render_calendar($month, $year);
@@ -44,6 +54,3 @@
 </body>
 
 </html>
-
-
-http://localhost:8080/calendar/index.php
