cab08af944ee5f8c6513b3747b36692027c86c36
Ituski Yamanaka <149759057+ituki1216@users.noreply.github.com>
2024-11-07 10:44:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -34,15 +34,12 @@
             
             <!-- 現在の年月を表示 -->
             <?php
-            // 現在の月と年をGETパラメータで取得（無ければデフォルトで現在の年月）
             $currentMonth = isset($_GET['ym']) ? $_GET['ym'] : date('Y-m'); 
 
             // 年月表示
             echo date('Y年n月', strtotime($currentMonth));
             ?>
-            <!-- 次月のリンク -->
             <a href="?ym=<?php 
-                // 次月のリンク作成
                 echo date('Y-m', strtotime('+1 month', strtotime($currentMonth))); 
             ?>">&gt;</a>
         </h2>
